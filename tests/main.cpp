016f425b77d07bce50059eefa52806ca3d98b4ae
baldurk <baldurk@baldurk.org>
2015-10-25 13:13:31 +0100

    Auto-number and print test indices

--- tests/main.cpp
+++ tests/main.cpp
@@ -36,7 +36,12 @@ bool GraphicsTest::Init(int argc, char **argv)
 
 #define STRINGIZE(a) #a
 
-#define TEST(idx, testname) int testname(int,char**); if(!strcmp(argv[1], #idx) || !strcmp(argv[1], STRINGIZE(testname))) { return testname(argc, argv); }
+#define TEST(testname) \
+	int testname(int,char**); \
+	_itoa_s(idx++, idxbuf, 10); \
+	printf("Test %s: %s\n", idxbuf, STRINGIZE(testname)); fflush(stdout); \
+	if(!strcmp(argv[1], idxbuf) || !strcmp(argv[1], STRINGIZE(testname))) \
+	{ printf("\n\n======\nRunning %s\n\n", STRINGIZE(testname)); fflush(stdout); return testname(argc, argv); }
 
 int main(int argc, char **argv)
 {
@@ -50,47 +55,50 @@ int main(int argc, char **argv)
 	// D3D11 tests
 	//////////////////////////////////////////////////////////////
 
+	char idxbuf[8] = {0};
+	int idx = 0;
+
 	// Just draws a simple triangle, using normal pipeline. Basic test
 	// that can be used for any dead-simple tests that don't require any
 	// particular API use
-	TEST(0, D3D11_Simple_Triangle);
+	TEST(D3D11_Simple_Triangle);
 
 	// Renders a lot of overlapping triangles
-	TEST(1, D3D11_Overdraw_Stress);
+	TEST(D3D11_Overdraw_Stress);
 
 	// Tests simple shader debugging identities by rendering many small
 	// triangles and performing one calculation to each to an F32 target
-	TEST(2, D3D11_Debug_Shader);
+	TEST(D3D11_Debug_Shader);
 
 	// Test using more than 8 compute shader UAVs (D3D11.1 feature)
-	TEST(3, D3D11_1_Many_UAVs);
+	TEST(D3D11_1_Many_UAVs);
 
 	// Test rendering into RTV mip levels
-	TEST(4, D3D11_Mip_RTV);
+	TEST(D3D11_Mip_RTV);
 
 	// Test repeatedly creating and destroying RTVs
-	TEST(5, D3D11_Many_RTVs);
+	TEST(D3D11_Many_RTVs);
 
 	// Test dispatching with one threadgroup count set to 0
-	TEST(6, D3D11_Empty_Compute_Dispatch);
+	TEST(D3D11_Empty_Compute_Dispatch);
 
 	// Test passing an array of float2 to make sure the interpolator
 	// packing is handled by shader debugging
-	TEST(7, D3D11_Array_Interpolator);
+	TEST(D3D11_Array_Interpolator);
 
 	// Test a drawcall of 0 size
-	TEST(8, D3D11_Empty_Drawcall);
+	TEST(D3D11_Empty_Drawcall);
 
 	// Test reading from structured buffers, with and without
 	// offsets
-	TEST(9, D3D11_Structured_Buffer_Read);
+	TEST(D3D11_Structured_Buffer_Read);
 
 	// Test setting some viewports that are empty, but enabled
-	TEST(10, D3D11_Empty_Viewports);
+	TEST(D3D11_Empty_Viewports);
 
 	// Test that just does a dispatch and some copies, for checking
 	// basic compute stuff
-	TEST(11, D3D11_Simple_Dispatch);
+	TEST(D3D11_Simple_Dispatch);
 
 	//////////////////////////////////////////////////////////////
 	// OpenGL tests
@@ -99,11 +107,11 @@ int main(int argc, char **argv)
 	// Just draws a simple triangle, using normal pipeline. Basic test
 	// that can be used for any dead-simple tests that don't require any
 	// particular API use
-	TEST(12, GL_Simple_Triangle);
+	TEST(GL_Simple_Triangle);
 
 	// Creates a MS FBO with one attachment created with an unsized
 	// internal format
-	TEST(13, GL_Unsized_MS_FBO_Attachment);
+	TEST(GL_Unsized_MS_FBO_Attachment);
 	
 	TEST_ERROR("%s is not a known test", argv[1]);
 
