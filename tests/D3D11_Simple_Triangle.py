aa3961e6fc53953addec31c2201d846f141d7617
baldurk <baldurk@baldurk.org>
2018-09-27 13:37:09 +0100

    Rename tests after the demo name

--- tests/D3D11_Simple_Triangle.py
+++ tests/D3D11_Simple_Triangle.py
@@ -1,9 +1,9 @@
 import rdtest as rdtest
 
 
-class GLSimpleTriangle(rdtest.TestCase):
+class D3D11_Simple_Triangle(rdtest.TestCase):
     def get_capture(self):
-        return rdtest.run_and_capture("demos_x64.exe", "GL_Simple_Triangle", 5)
+        return rdtest.run_and_capture("demos_x64.exe", "D3D11_Simple_Triangle", 5)
 
     def check_capture(self, controller):
         self.check_final_backbuffer(controller)
