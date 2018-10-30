6ebe37793d162c0aaaad9017c16d91cf6050294d
baldurk <baldurk@baldurk.org>
2018-10-30 17:51:23 +0000

    Choose SRGB backbuffer for vulkan

--- demos/vk/vk_test.cpp
+++ demos/vk/vk_test.cpp
@@ -749,7 +749,7 @@ bool VulkanGraphicsTest::createSwap()
 
   for(const VkSurfaceFormatKHR &f : formats)
   {
-    if(f.format == VK_FORMAT_B8G8R8A8_UNORM && f.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR)
+    if(f.format == VK_FORMAT_B8G8R8A8_SRGB && f.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR)
     {
       format = f;
       break;
@@ -758,7 +758,7 @@ bool VulkanGraphicsTest::createSwap()
 
   if(format.format == VK_FORMAT_UNDEFINED)
   {
-    format.format = VK_FORMAT_B8G8R8A8_UNORM;
+    format.format = VK_FORMAT_B8G8R8A8_SRGB;
     format.colorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR;
   }
 
